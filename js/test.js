98c4385dce755fb29a9b7c07bd02ea6f0ff3c682
Peter Farmer <pfarmer@gmail.com>
2011-09-22 11:18:59 +0100

    Initial import

---
+++ js/test.js
@@ -0,0 +1,74 @@
+function do_counter() {
+    $('#counter1').countdown({
+        startTime: "9",
+        stepTime: 1,
+        digitImages: 6,
+        digitWidth: 53,
+        digitHeight: 77,
+        image: "http://assets.digidayoff.com/images/digits.png"
+    });
+
+    $('#counter2').countdown({
+        startTime: "60",
+        stepTime: 1,
+        digitImages: 6,
+        digitWidth: 53,
+        digitHeight: 77,
+        image: "http://assets.digidayoff.com/images/digits.png"
+    });
+
+    $('#counter3').countdown({
+        startTime: "1:1",
+        stepTime: 1,
+        digitImages: 6,
+        digitWidth: 53,
+        digitHeight: 77,
+        image: "http://assets.digidayoff.com/images/digits.png"
+    });
+
+    $('#counter4').countdown({
+        startTime: "11:11",
+        stepTime: 1,
+        digitImages: 6,
+        digitWidth: 53,
+        digitHeight: 77,
+        image: "http://assets.digidayoff.com/images/digits.png"
+    });
+
+    $('#counter5').countdown({
+        startTime: "1:1:1",
+        stepTime: 1,
+        digitImages: 6,
+        digitWidth: 53,
+        digitHeight: 77,
+        image: "http://assets.digidayoff.com/images/digits.png"
+    });
+
+    $('#counter6').countdown({
+        startTime: "11:11:11",
+        stepTime: 1,
+        digitImages: 6,
+        digitWidth: 53,
+        digitHeight: 77,
+        image: "http://assets.digidayoff.com/images/digits.png"
+    });
+
+    $('#counter7').countdown({
+        startTime: "1:1:1:1",
+        stepTime: 1,
+        digitImages: 6,
+        digitWidth: 53,
+        digitHeight: 77,
+        image: "http://assets.digidayoff.com/images/digits.png"
+    });
+
+    $('#counter7').countdown({
+        startTime: "11:11:11:11",
+        stepTime: 1,
+        digitImages: 6,
+        digitWidth: 53,
+        digitHeight: 77,
+        image: "http://assets.digidayoff.com/images/digits.png"
+    });
+
+};
