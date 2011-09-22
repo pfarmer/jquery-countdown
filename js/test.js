aad6bff43775ec824448837bf7eb94717bb13d69
Peter Farmer <pfarmer@gmail.com>
2011-09-22 11:23:53 +0100

    Update.

--- js/test.js
+++ js/test.js
@@ -1,5 +1,5 @@
 function do_counter() {
-    $('#counter1').countdown({
+    $('#counter2').countdown({
         startTime: "9",
         stepTime: 1,
         digitImages: 6,
@@ -8,7 +8,7 @@ function do_counter() {
         image: "https://github.com/pfarmer/jquery-countdown/raw/master/img/digits.png"
     });
 
-    $('#counter2').countdown({
+    $('#counter3').countdown({
         startTime: "60",
         stepTime: 1,
         digitImages: 6,
@@ -17,7 +17,7 @@ function do_counter() {
         image: "https://github.com/pfarmer/jquery-countdown/raw/master/img/digits.png"
     });
 
-    $('#counter3').countdown({
+    $('#counter4').countdown({
         startTime: "1:1",
         stepTime: 1,
         digitImages: 6,
@@ -26,7 +26,7 @@ function do_counter() {
         image: "https://github.com/pfarmer/jquery-countdown/raw/master/img/digits.png"
     });
 
-    $('#counter4').countdown({
+    $('#counter5').countdown({
         startTime: "11:11",
         stepTime: 1,
         digitImages: 6,
@@ -35,7 +35,7 @@ function do_counter() {
         image: "https://github.com/pfarmer/jquery-countdown/raw/master/img/digits.png"
     });
 
-    $('#counter5').countdown({
+    $('#counter6').countdown({
         startTime: "1:1:1",
         stepTime: 1,
         digitImages: 6,
@@ -44,7 +44,7 @@ function do_counter() {
         image: "https://github.com/pfarmer/jquery-countdown/raw/master/img/digits.png"
     });
 
-    $('#counter6').countdown({
+    $('#counter7').countdown({
         startTime: "11:11:11",
         stepTime: 1,
         digitImages: 6,
@@ -53,7 +53,7 @@ function do_counter() {
         image: "https://github.com/pfarmer/jquery-countdown/raw/master/img/digits.png"
     });
 
-    $('#counter7').countdown({
+    $('#counter8').countdown({
         startTime: "1:1:1:1",
         stepTime: 1,
         digitImages: 6,
@@ -62,7 +62,7 @@ function do_counter() {
         image: "https://github.com/pfarmer/jquery-countdown/raw/master/img/digits.png"
     });
 
-    $('#counter7').countdown({
+    $('#counter9').countdown({
         startTime: "11:11:11:11",
         stepTime: 1,
         digitImages: 6,
